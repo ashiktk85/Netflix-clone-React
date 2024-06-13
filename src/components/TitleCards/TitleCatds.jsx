de0ad3004d5546f404b2b0d5034419ee566bc190
ashiktk85 <111106558+ashiktk85@users.noreply.github.com>
2024-06-13 21:03:39 +0530

    first commit

---
+++ src/components/TitleCards/TitleCatds.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './TitleCatds.css'
+
+const TitleCatds = () => {
+  return (
+    <div className='titlecards'>
+      
+    </div>
+  )
+}
+
+export default TitleCatds
